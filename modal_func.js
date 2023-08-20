675276e96bb825854b1a1aec3b649276326a3558
jsw4795 <33516979+jsw4795@users.noreply.github.com>
2023-08-20 22:57:01 +0900

    Add files via upload

---
+++ modal_func.js
@@ -0,0 +1,18 @@
+function popOpen() {
+
+    var modalPop = $('.modal_wrap');
+    var modalBg = $('.modal_bg');
+
+    $(modalPop).show();
+    $(modalBg).show();
+
+}
+
+function popClose() {
+    var modalPop = $('.modal_wrap');
+    var modalBg = $('.modal_bg');
+
+    $(modalPop).hide();
+    $(modalBg).hide();
+
+}
